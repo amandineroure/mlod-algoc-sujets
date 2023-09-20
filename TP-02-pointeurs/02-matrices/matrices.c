be6ae5cfeb1eeb521670f50f6b4179a4ba07f083
Amandine <amandineroure@gmail.com>
2023-09-20 09:49:35 +0200

    update

--- TP-02-pointeurs/02-matrices/matrices.c
+++ TP-02-pointeurs/02-matrices/matrices.c
@@ -5,16 +5,6 @@
 /*gcc -Wall -o matrices matrices.c
 .\matrices.exe*/
 
-int main(void) {
-//matrices en ligne * colonne
-int64_t matrice1[][SIZE]={{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5},{1,2,3,4,5}};
-int64_t matrice2[][SIZE]={{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10},{6,7,8,9,10}};
-int64_t matriceResultat[SIZE][SIZE];
-matrix_mult(matriceResultat,matrice1,matrice2);
-matrix_print(matriceResultat);
-return EXIT_SUCCESS;
-}
-
 void matrix_mult(int64_t matriceResultat[][SIZE], int64_t matrice1[][SIZE], int64_t matrice2[][SIZE]){
     for (int i=0; i< SIZE; i++)
     {
